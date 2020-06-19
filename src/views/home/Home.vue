f381b836676cd328f29cee32bf564f545c6e2ab0
Jason <Jason20160606@hotmail.com>
2020-06-19 22:55:12 +0800

    tarbar提交

---
+++ src/views/home/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="home"> 
+    <h2>首页</h2>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"        
+    }
+</script>
+
+<style scoped>
+  #home {
+    padding-top: 44px;
+    height: 100vh;
+    position: relative;
+  }
+
+  .home-nav-bar {
+    background-color: var(--color-tint);
+    color:#fff;
+
+    position: fixed;
+    left: 0;
+    right:0;
+    top: 0;
+    z-index: 9;
+
+  }
+
+  .home-tab-control{
+    /*两个要混合使用*/
+    position: sticky;
+    top: 43px;/*顶部navbar的高度*/
+    z-index: 9;
+  }
+
+  .home-scroller{
+    /*height:300px;*/
+    overflow: hidden;
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    right: 0;
+    left: 0;
+  }
+
+
+
+</style>
